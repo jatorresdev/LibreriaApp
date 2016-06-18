967756026ffbe865d6c49fa751ac858022441ce3
Angel Torres <jatorresdev@gmail.com>
2016-06-18 16:23:16 -0500

    Se añade validación de campo para buscar libro

--- app/src/main/java/com/example/aprendiz/libreriaapp/PreModificar.java
+++ app/src/main/java/com/example/aprendiz/libreriaapp/PreModificar.java
@@ -3,8 +3,10 @@ package com.example.aprendiz.libreriaapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by APRENDIZ on 10/06/2016.
@@ -22,9 +24,17 @@ public class PreModificar extends AppCompatActivity {
 
     // Metodo para el boton premodificar
     public void modificarOnClick(View mv) {
-        Intent i = new Intent(this, Modificar.class);
         txtIdLibro = (EditText) findViewById(R.id.txtIdLibro);
-        i.putExtra("id", txtIdLibro.getText().toString());
-        startActivity(i);
+        String _id = txtIdLibro.getText().toString().trim();
+
+        if (!_id.equals("")) {
+            Intent i = new Intent(this, Modificar.class);
+            i.putExtra("id", _id);
+            startActivity(i);
+        } else {
+            txtIdLibro.setText("");
+            Toast.makeText(getApplicationContext(), "Valor invalido, verifique", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
